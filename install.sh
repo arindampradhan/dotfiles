b75c3a0d4c8bdabd1e08e0dd35f645a8b8b2e735
arindampradhan <arindampradhan10@gmail.com>
2016-04-05 12:14:00 +0530

    updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 add-apt-repository -y ppa:webupd8team/java
-add-apt-repository -y ppa:webupd8team/sublime-text-3
 add-apt-repository -y ppa:linrunner/tlp
 add-apt-repository -y ppa:fossfreedom/byzanz
 apt-get update
@@ -9,8 +8,6 @@ apt-get -y install mplayer
 apt-get -y install Preload
 apt-get -y install curl
 apt-get -y install libcurl3 libcurl3-dev php5-curl
-apt-get -y install youtube-dl
-apt-get -y install python-pip
 apt-get -y install python-software-properties
 apt-get -y install ubuntu-restricted-extras
 apt-get -y install flashplugin-installer
@@ -19,25 +16,13 @@ apt-get -y install git
 apt-get -y install openjdk-7-jre
 apt-get -y install openjdk-7-jdk
 apt-get -y install icedtea-7-plugin
-apt-get -y install sublime-text-installer
 apt-get -y install virtualbox
 apt-get -y install tlp tlp-rdw
 apt-get -y install redshift
 apt-get -y install byzanz
 tlp start
 
-apt-get -y install zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | ZSH=~/.dotfiles/zsh bash
-chsh -s $(which zsh) $(whoami)
-echo
-echo "zsh"
-echo "your shell is now"
-which zsh
-echo "Please logout and than login"
-echo
 
-easy_install pip
 pip install --upgrade
 pip install ipython
 pip install flask
@@ -49,6 +34,9 @@ pip install lxml
 pip install thefuck
 pip install youtube-dl
 pip install beautifulsoup4
-pip install cheesy
-pip install pok
 pip install virtualenvwrapper
+
+
+apt-get -y install zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+echo "Please logout and than login!! for zsh to be installed..."
