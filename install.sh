1cd5bb82a7f14ad398c1ace6cbbafa703dc1d027
Arindam Pradhan <Arindam Pradhan>
2016-08-18 23:34:45 +0530

    moved zsh script to end

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ apt-get -y install curl
 apt-get -y install libcurl3 libcurl3-dev php5-curl
 apt-get -y install python-software-properties
 apt-get -y install python-dev
-apt-get -y install ubuntu-restricted-extras
 apt-get -y install rar
 apt-get -y install git
 apt-get -y install byzanz
@@ -35,20 +34,20 @@ pip install requests
 pip install virtualenv
 pip install youtube-dl
 pip install virtualenvwrapper
-
+pip install speedtest-cli
 
 # nodejs | nvm
 apt-get -y install nodejs
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 
 
-apt-get -y install zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-echo "adding zsh scripts to .zshrc"
-cat ./zsh/.zsh-add >> $HOME/.zshrc
-
-
 mv ./vim/.vim ./vim/.vimrc $HOME
 mv npm/.npmrc $HOME
 mv ipython/.pypirc $HOME
 mv git/.gitconfig $HOME
+
+# install this in the end
+apt-get -y install zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+echo "adding zsh scripts to .zshrc"
+cat ./zsh/.zshrc-add.sh >> $HOME/.zshrc
