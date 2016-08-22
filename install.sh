fb5a9f71c5d00b37804e6d4de538bd3fb0a3b406
Arindam Pradhan <Arindam Pradhan>
2016-08-22 18:32:49 +0530

    changed order of nvm

--- install.sh
+++ install.sh
@@ -8,14 +8,15 @@ apt-get -y upgrade
 
 
 # apt-get dependencies
+apt-get -y install git
 apt-get -y install curl
 apt-get -y install libcurl3 libcurl3-dev php5-curl
 apt-get -y install python-software-properties
 apt-get -y install python-dev
 apt-get -y install rar
-apt-get -y install git
 apt-get -y install byzanz
 apt-get -y install vim
+apt-get -y install zsh
 
 
 # install pip
@@ -39,8 +40,7 @@ pip install speedtest-cli
 # nodejs | nvm
 apt-get -y install nodejs
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
-nvm install node
-nvm use stable
+
 
 mv ./vim/.vim ./vim/.vimrc $HOME
 mv npm/.npmrc $HOME
@@ -48,7 +48,11 @@ mv ipython/.pypirc $HOME
 mv git/.gitconfig $HOME
 
 # install this in the end
-apt-get -y install zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo "adding zsh scripts to .zshrc"
-cat ./zsh/.zshrc-add.sh >> $HOME/.zshrc
+cat ./zsh/.zshrc-add >> $HOME/.zshrc
+nvm install node
+nvm use stable
+
+# U will need this command the most
+# sudo chown -R username:group directory
